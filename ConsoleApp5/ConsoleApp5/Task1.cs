e196418c2cfbf36588c6806e291e1cbba301c599
Dm5Xia9 <68317543+Dm5Xia9@users.noreply.github.com>
2021-04-07 15:52:02 +0500

    key update

---
+++ ConsoleApp5/ConsoleApp5/Task1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Task1
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Вводите количество символов массива X ");
+            int ArrCountX = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Вводите количество символов массива Y ");
+            int ArrCountY = Convert.ToInt32(Console.ReadLine());
+            Random rand = new Random();
+            int[] arrayX = randomArr(ArrCountX, rand, 0, 10000);
+            int[] arrayY = randomArr(ArrCountY, rand, 0, 10000);
+            int minX = MinArr(arrayX);
+            int minY = MinArr(arrayY);
+            Console.WriteLine($"Разность между минимальными элементами равна {minX - minY}");
+            Console.ReadKey();
+        }
+        static int[] randomArr(int Length, Random rand, int min, int max)
+        {
+            int[] array = new int[Length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(min, max);
+            }
+            return (array);
+        }
+        static int MinArr(int[] arr)
+        {
+            int min = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+            return (min);
+        }
+    }
+}
---
+++ ConsoleApp5/ConsoleApp5/Task2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Task2
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int[] arrayA = randomArr(20, rand, -100, 100);
+            List<int> listB = new List<int>();
+            Console.Write("Массив A ");
+            foreach (var item in arrayA)
+            {
+                if (item > 0)
+                {
+                    listB.Add(item);
+                }
+                Console.Write($"{item} ");
+            }
+            int[] arrayB = listB.ToArray();
+            Console.WriteLine(" ");
+            Console.Write("Массив B ");
+            int pB = 1;
+            foreach (var item in arrayB)
+            {
+                pB *= item;
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine($"Количество положительных элементов: {arrayB.Length} ");
+            Console.WriteLine($"Произведение положительных элементов: {pB} ");
+            Console.ReadKey();
+        }
+        static int[] randomArr(int Length, Random rand, int min, int max)
+        {
+            int[] array = new int[Length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(min, max);
+            }
+            return (array);
+        }
+    }
+}
